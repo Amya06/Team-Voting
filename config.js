1860e593eda85ef5714a476b9e5f66e8ea4d40aa
Amya06 <96235429+Amya06@users.noreply.github.com>
2023-01-21 15:46:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyALCBG4R1a6OYroUdeGt2iiUTiS2kqqsDI",
+  authDomain: "pro-58-7b313.firebaseapp.com",
+  databaseURL: "https://pro-58-7b313-default-rtdb.firebaseio.com",
+  projectId: "pro-58-7b313",
+  storageBucket: "pro-58-7b313.appspot.com",
+  messagingSenderId: "886785477584",
+  appId: "1:886785477584:web:3bfec08915a56fdd7ec7d5"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
